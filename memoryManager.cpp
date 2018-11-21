7f91c4b3b413c6ef0fba8f001b6072611d77727c
Aaron Garcia <aarongarciag97@gmail.com>
2018-11-20 18:17:59 -0600

    Added definition of global variables

--- memoryManager.cpp
+++ memoryManager.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <queue>
 
 using namespace std;
+
+// You can change this variables during compilation
+#ifndef PAGE_SIZE
+#define PAGE_SIZE 16
+#endif
+
+#ifndef REAL_SIZE
+#define REAL_SIZE 2048
+#endif
+
+#ifndef RESERVE_SIZE
+#define RESERVE_SIZE 4096
+#endif
 
 int swapIns = 0;
 int swapOuts = 0;
 int pageFaults = 0;
 
+queue<int> paginasDispM;
+queue<int> paginasDispS;
+
 struct ProcessInfo{
   int pid;
   bool bitRef;
   int timeStamp, pageFaults;
-  vector <int> pages;
+  vector <int> pagesUsed;
 
   //Dummy constructor, to test data
   ProcessInfo(int _pid, bool _bitRef, int _timeStamp, int _pageFaults){
@@ -23,10 +41,14 @@ struct ProcessInfo{
 
 };
 
+unordered_map<int, ProcessInfo> tablaMem;
+
 int main(int argc, char *argv[]){
 
   ProcessInfo *p = new ProcessInfo(1, 0, 0, 0);
-  cout << p->pid << " " << p->bitRef << " " << p->timeStamp << " " << p->pageFaults << " " << endl;
+  cout << p->pid << " " << p->bitRef << " " << p->timeStamp << " " << p->pageFaults  << endl;
+
+  cout << PAGE_SIZE << " " << REAL_SIZE << " " << RESERVE_SIZE << endl;
 
   return 0;
 }
