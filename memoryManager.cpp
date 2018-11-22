cf8ea61f8c918cabc79367c6f961704ed95b45eb
Aaron Garcia <aarongarciag97@gmail.com>
2018-11-22 00:40:18 -0600

    Accessing process when it's on REAL memory works.

--- memoryManager.cpp
+++ memoryManager.cpp
@@ -374,6 +374,62 @@ int loadProcess(int &n, int &pid){
 
 }
 
+/*
+  Function which receives pid and address, and prints virtual memory
+  address and real memory address.
+*/
+int showAddresses(int &address, ProcessInfo* &process){
+
+  int realAddress;
+  int virtualAddress;
+
+  // Gets you page in which address is located on its own virtual memory
+  virtualAddress = address / PAGE_SIZE;
+
+  // Gives you page in which address is located on real memory
+  realAddress = process->pagesUsed[virtualAddress];
+
+  cout << "Virtual address: " << virtualAddress << "." << endl;
+  cout << "Real Memory address: " << realAddress << "." << endl;
+
+  return 1;
+
+}
+
+/*
+  Function which receives an address to be accessed and a pid of a process,
+  whose address is going to be accessed.
+*/
+int accessProcess(int &address, int &pid){
+
+  // Check that process is in memory
+  if( tablaMem.find(pid) == tablaMem.end() ){
+    cout << "Process(PID=" << pid << ") doesn't exist." << endl;
+    return -1;
+  }
+
+
+  ProcessInfo *process = tablaMem[pid];
+  int bitRef = process->bitRef;
+
+  // Check if process is in Reserve or Real memory
+  if( bitRef == 0 ){ // Real
+
+    cout << "Process(PID=" << pid << ") is in REAL Memory" << endl;
+    showAddresses(address, process);
+
+  } else { // Reserve
+
+    cout << "Process(PID=" << pid << ") is in RESERVE Memory" << endl;
+    // send process from reserve to real memory
+    //sendProcessToMemory(pid);
+    // showAddresses(address, pid);
+
+  }
+
+  return 1;
+}
+
 int main(int argc, char *argv[]){
 
   int n, pid, address, type;
@@ -402,6 +458,7 @@ int main(int argc, char *argv[]){
     case 'A' :  // Access process (A d p m) => (action virtualAddress pid type)
       cin >> address >> pid >> type;
       cout << "Acessing process..." << endl;
+      accessProcess(address, pid);
       break;
 
     case 'L' :  // Free every page of process (L p) => (action pid)
